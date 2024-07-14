a2f4b1c843664a4737d879eb6cb5c9b418045af0
pvornelas <pviniciusornelas@gmail.com>
2024-07-14 14:28:53 -0300

    Implementa Login, Autenticação de usuário

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,7 +6,7 @@ namespace API.Data;
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions options) : base(options)
-    {        
+    {
     }
 
     public DbSet<AppUser> Users { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,34 +1,18 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
-app.UseCors(builder => builder.AllowAnyOrigin());
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.MapControllers();
 
